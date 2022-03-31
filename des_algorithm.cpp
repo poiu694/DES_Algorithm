962887de15dbd328fba610ca9f5d6639c6ee9719
Lee <43488305+poiu694@users.noreply.github.com>
2022-03-31 11:44:59 +0900

    [Fix] fix log code position

--- des_algorithm.cpp
+++ des_algorithm.cpp
@@ -28,14 +28,13 @@ string des_algorhtim(string plaintext, string key)
 	sbox_key = get_sbox_key(xor_key);
 	sbox_key = get_sbox_permutation(sbox_key);
 	xor_32 = xor_bit32(plain_left_key, sbox_key);
-	printf("%10s %10s %10s %10s\n", "round", "left", "right", "round key");
-	print_log(1, get_bits_to_hexa_bits(plain_left_key), get_bits_to_hexa_bits(plain_right_key), des_key);
 	plain_left_key = plain_right_key;
 	plain_right_key = xor_32;
+	printf("%10s %10s %10s %10s\n", "round", "left", "right", "round key");
+	print_log(1, get_bits_to_hexa_bits(plain_left_key), get_bits_to_hexa_bits(plain_right_key), des_key);
 	for (int i=2; i<=16; i++)
 	{
 		des_key = generate_key(left_key, right_key, i);
-		print_log(i, get_bits_to_hexa_bits(plain_left_key), get_bits_to_hexa_bits(plain_right_key), des_key);
 		expansion_key = expansion_right_key(plain_right_key);
 		xor_key = xor_bit48(expansion_key, get_entire_bit_key(des_key));
 		sbox_key = get_sbox_key(xor_key);
@@ -43,6 +42,10 @@ string des_algorhtim(string plaintext, string key)
 		xor_32 = xor_bit32(plain_left_key, sbox_key);
 		plain_left_key = plain_right_key;
 		plain_right_key = xor_32;
+		if (i == 16)
+			print_log(i, get_bits_to_hexa_bits(plain_right_key), get_bits_to_hexa_bits(plain_left_key), des_key);
+		else
+			print_log(i, get_bits_to_hexa_bits(plain_left_key), get_bits_to_hexa_bits(plain_right_key), des_key);
 	}
 	plain64 = plain_right_key + plain_left_key;
 	encrypted_key = get_bits_to_hexa_bits(inverse_ip(plain64));
